595141b48084eb23c87ed9a999189a5a7b108d1e
Singh <437038@cognizant.com>
2019-06-25 14:43:17 +0530

    application running

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,32 @@
+import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatInputModule} from '@angular/material/input';
+import {MatButtonModule} from '@angular/material/button';
+import { FormsModule } from '@angular/forms';
+
+import { HttpClientModule } from '@angular/common/http'
+import { NotesService } from './notes.service';
+import { HeaderComponent } from './header/header.component';
+
 @NgModule({
-  declarations: [ ],
-  imports: [ ],
-  providers: [ ],
-  bootstrap: [ ]
+  declarations: [
+    AppComponent,HeaderComponent
+  ],
+  imports: [
+    BrowserModule,
+    BrowserAnimationsModule,
+    MatToolbarModule,
+    MatInputModule,
+    MatButtonModule,
+    FormsModule,
+    HttpClientModule
+  ],
+  providers: [NotesService],
+  bootstrap: [AppComponent]
 })
 export class AppModule { }
