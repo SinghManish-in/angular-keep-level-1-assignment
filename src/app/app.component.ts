f6033691f3e1b5c528e20db2d6e4e7df6a08022c
Singh <437038@cognizant.com>
2019-06-25 16:35:05 +0530

    working app

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import {NotesService} from './notes.service'
+import { Note } from './note'
 
 @Component({
   selector: 'app-root',
@@ -7,6 +9,35 @@ import { Component } from '@angular/core';
 })
 
 export class AppComponent {
+  title = 'note-app';
   errMessage: string;
+  notes: Array<Note>;
+  note: Note = new Note("","");
 
+  constructor(private notesService: NotesService){
+
+    this.notesService.getNotes().subscribe(data=>{
+        this.notes = data;
+    }, err=>{
+      this.errMessage = err.message;
+    })
+  }
+
+addNote(note: Note): void{
+
+  if(this.note.title == "" || this.note.text == "") {
+
+    this.errMessage = "Need both name and department";
+  } else {
+
+    this.notesService.addNote(this.note).subscribe(data=>{},error=>{
+
+      this.errMessage = "Could not add Student";
+    });
+
+    this.errMessage = "";
+    this.notes.push(this.note);
+    this.note = new Note("","");
+  }
+  
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,7 +7,7 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import {MatToolbarModule} from '@angular/material/toolbar';
 import {MatInputModule} from '@angular/material/input';
 import {MatButtonModule} from '@angular/material/button';
-import { FormsModule } from '@angular/forms';
+import {FormsModule} from '@angular/forms';
 
 import { HttpClientModule } from '@angular/common/http'
 import { NotesService } from './notes.service';
