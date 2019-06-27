00207726f9b9490da420cb8039438e2ee2abde5d
Singh <437038@cognizant.com>
2019-06-27 12:34:25 +0530

    all test cases passed

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { NotesService } from './notes.service'
 import { Note } from './note'
 
@@ -9,12 +9,14 @@ import { Note } from './note'
   styleUrls: ['./app.component.css']
 })
 
-export class AppComponent {
+export class AppComponent implements OnInit {
+
   errMessage: string;
   notes: Array<Note>;
   note: Note = new Note("", "");
-
-  constructor(private notesService: NotesService) {
+  constructor(private notesService: NotesService) { }
+  
+  ngOnInit(){
     this.notesService.getNotes().subscribe(data => {
       this.notes = data;
     }, err => {
@@ -22,16 +24,15 @@ export class AppComponent {
     })
   }
 
+
   addNote(note: Note): void {
     if (this.note.title == "" || this.note.text == "") {
       this.errMessage = "Title and Text both are required fields";
     } else {
-      this.notesService.addNote(this.note).subscribe(data => { }, error => {
-        this.errMessage = "Could not add note";
+      this.notesService.addNote(this.note).subscribe(data => { }, err => {
+        this.errMessage = err.message;
       });
-      this.errMessage = "";
       this.notes.push(this.note);
-      this.note = new Note("", "");
     }
   }
 }
